1ecce977a38beb9cabb770b9124580e59f2aec12
satyabartg <ak4304015@gmail.com>
2024-02-14 00:09:31 +0530

    127:Create save Post feature

---
+++ src/main/java/com/mypostgre/mycode28/Mycode28Application.java
@@ -0,0 +1,13 @@
+package com.mypostgre.mycode28;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mycode28Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mycode28Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mypostgre/mycode28/Mycode28ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mypostgre.mycode28;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mycode28ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
